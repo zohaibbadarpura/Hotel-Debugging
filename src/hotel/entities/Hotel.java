993289bd86de1cad4a4ca4d5d14ebf536a4e49bd
Zohaib Badarpura <zohaibbadarpura@gmail.com>
2018-10-06 23:12:45 +1000

    First Project Code
    Initial Code

---
+++ src/hotel/entities/Hotel.java
@@ -0,0 +1,132 @@
+package hotel.entities;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import hotel.credit.CreditCard;
+import hotel.utils.IOUtils;
+
+public class Hotel {
+	
+	private Map<Integer, Guest> guests;
+	public Map<RoomType, Map<Integer,Room>> roomsByType;
+	public Map<Long, Booking> bookingsByConfirmationNumber;
+	public Map<Integer, Booking> activeBookingsByRoomId;
+	
+	
+	public Hotel() {
+		guests = new HashMap<>();
+		roomsByType = new HashMap<>();
+		for (RoomType rt : RoomType.values()) {
+			Map<Integer, Room> rooms = new HashMap<>();
+			roomsByType.put(rt, rooms);
+		}
+		bookingsByConfirmationNumber = new HashMap<>();
+		activeBookingsByRoomId = new HashMap<>();
+	}
+
+	
+	public void addRoom(RoomType roomType, int id) {
+		IOUtils.trace("Hotel: addRoom");
+		for (Map<Integer, Room> rooms : roomsByType.values()) {
+			if (rooms.containsKey(id)) {
+				throw new RuntimeException("Hotel: addRoom : room number already exists");
+			}
+		}
+		Map<Integer, Room> rooms = roomsByType.get(roomType);
+		Room room = new Room(id, roomType);
+		rooms.put(id, room);
+	}
+
+	
+	public boolean isRegistered(int phoneNumber) {
+		return guests.containsKey(phoneNumber);
+	}
+
+	
+	public Guest registerGuest(String name, String address, int phoneNumber) {
+		if (guests.containsKey(phoneNumber)) {
+			throw new RuntimeException("Phone number already registered");
+		}
+		Guest guest = new Guest(name, address, phoneNumber);
+		guests.put(phoneNumber, guest);		
+		return guest;
+	}
+
+	
+	public Guest findGuestByPhoneNumber(int phoneNumber) {
+		Guest guest = guests.get(phoneNumber);
+		return guest;
+	}
+
+	
+	public Booking findActiveBookingByRoomId(int roomId) {
+		Booking booking = activeBookingsByRoomId.get(roomId);;
+		return booking;
+	}
+
+
+	public Room findAvailableRoom(RoomType selectedRoomType, Date arrivalDate, int stayLength) {
+		IOUtils.trace("Hotel: checkRoomAvailability");
+		Map<Integer, Room> rooms = roomsByType.get(selectedRoomType);
+		for (Room room : rooms.values()) {
+			IOUtils.trace(String.format("Hotel: checking room: %d",room.getId()));
+			if (room.isAvailable(arrivalDate, stayLength)) {
+				return room;
+			}			
+		}
+		return null;
+	}
+
+	
+	public Booking findBookingByConfirmationNumber(long confirmationNumber) {
+		return bookingsByConfirmationNumber.get(confirmationNumber);
+	}
+
+	
+	public long book(Room room, Guest guest, 
+			Date arrivalDate, int stayLength, int occupantNumber,
+			CreditCard creditCard) {
+		
+		Booking booking = room.book(guest, arrivalDate, stayLength, occupantNumber, creditCard);
+		long confirmationNumber = booking.getConfirmationNumber();
+		bookingsByConfirmationNumber.put(confirmationNumber, booking);
+		return confirmationNumber;		
+	}
+
+	
+	public void checkin(long confirmationNumber) {
+		Booking booking = bookingsByConfirmationNumber.get(confirmationNumber);
+		if (booking == null) {
+			String message = String.format("Hotel: checkin: No booking found for confirmation number %d", confirmationNumber);
+			throw new RuntimeException(message);
+		}
+		int roomId = booking.getRoomId();
+		
+		booking.checkIn();
+		activeBookingsByRoomId.put(roomId, booking);
+	}
+
+
+	public void addServiceCharge(int roomId, ServiceType serviceType, double cost) {
+		Booking booking = activeBookingsByRoomId.get(roomId);
+		if (booking == null) {
+			String mesg = String.format("Hotel: addServiceCharge: no booking present for room id : %d", roomId);
+			throw new RuntimeException(mesg);
+		}
+		booking.addServiceCharge(serviceType, cost);
+	}
+
+	
+	public void checkout(int roomId) {
+		Booking booking = activeBookingsByRoomId.get(roomId);
+		if (booking == null) {
+			String mesg = String.format("Hotel: checkout: no booking present for room id : %d", roomId);
+			throw new RuntimeException(mesg);
+		}
+		booking.checkOut();
+	}
+
+
+}
---
+++ src/hotel/service/RecordServiceCTL.java
@@ -0,0 +1,79 @@
+package hotel.service;
+
+import hotel.entities.Booking;
+import hotel.entities.Hotel;
+import hotel.entities.ServiceCharge;
+import hotel.entities.ServiceType;
+import hotel.utils.IOUtils;
+
+public class RecordServiceCTL {
+	
+	private static enum State {ROOM, SERVICE, CHARGE, CANCELLED, COMPLETED};
+	
+	private Hotel hotel;
+	private RecordServiceUI recordServiceUI;
+	private State state;
+	
+	private Booking booking;
+	private int roomNumber;
+
+
+	public RecordServiceCTL(Hotel hotel) {
+		this.recordServiceUI = new RecordServiceUI(this);
+		state = State.ROOM;
+		this.hotel = hotel;
+	}
+
+	
+	public void run() {		
+		IOUtils.trace("PayForServiceCTL: run");
+		recordServiceUI.run();
+	}
+
+
+	public void roomNumberEntered(int roomNumber) {
+		if (state != State.ROOM) {
+			String mesg = String.format("PayForServiceCTL: roomNumberEntered : bad state : %s", state);
+			throw new RuntimeException(mesg);
+		}
+		booking = hotel.findActiveBookingByRoomId(roomNumber);
+		if (booking == null) {
+			String mesg = String.format("No active booking for room id: %d", roomNumber);
+			recordServiceUI.displayMessage(mesg);
+		}
+		else {
+			this.roomNumber = roomNumber;
+			state = State.SERVICE;
+			recordServiceUI.setState(RecordServiceUI.State.SERVICE);
+		}
+	}
+	
+	
+	public void serviceDetailsEntered(ServiceType serviceType, double cost) {
+		if (state != State.SERVICE) {
+			String mesg = String.format("PayForServiceCTL: serviceDetailsEntered : bad state : %s", state);
+			throw new RuntimeException(mesg);
+		}
+		hotel.addServiceCharge(roomNumber, serviceType, cost);
+		
+		recordServiceUI.displayServiceChargeMessage(roomNumber, cost, serviceType.getDescription());
+		state = State.COMPLETED;
+		recordServiceUI.setState(RecordServiceUI.State.COMPLETED);
+	}
+
+
+	public void cancel() {
+		recordServiceUI.displayMessage("Pay for service cancelled");
+		state = State.CANCELLED;
+		recordServiceUI.setState(RecordServiceUI.State.CANCELLED);
+	}
+
+
+	public void completed() {
+		recordServiceUI.displayMessage("Pay for service completed");
+	}
+
+
+	
+
+}
